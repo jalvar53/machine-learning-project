60b80dfaa2a6e57db97105e64adba608f9faffa9
camilo <cam.912@hotmail.com>
2017-10-12 09:24:11 -0500

    movimiento mejorado

--- Turtlebot.py
+++ Turtlebot.py
@@ -15,19 +15,47 @@ class Turtlebot:
         self.svm = SvmModel()
         self.x = 0
 
-    def move(self,p):
+    def move(self,p,s):
         msg = Twist()
-        #print("%d, %d, %d, %d" %(p[100],p[101],p[102],p[103]))
-        print(p[84:96])
-        #if(p[100]*p[101]*p[102]*p[103] > 0):
-        if(p[88]*p[89]*p[90]*p[91] > 0):
+        print(p[84-s:96-s])
+        print(p[96-s:108-s])
+        left = sum(p[84-s:88-s])
+        central = sum(p[88-s:92-s])
+        rigth = sum(p[92-s:96-s])
+        lefts = sum(p[96-s:100-s])
+        rigths = sum(p[104-s:108-s])
+        if(central > 2):
             #msg.linear.x = 0.2
             #msg.angular.z = 0
             print("moverse alante")
-        else:
+        elif(left > 2 or lefts > 2):
             # msg.linear.x = 0
             # msg.angular.z = 0.2
-            print("hay obstaculo")
+            print("mover a la izquierda")
+        elif(rigth > 2 or rigths > 2):
+            # msg.linear.x = 0
+            # msg.angular.z = -0.2
+            print("mover a la derecha")
+        elif(left > 1 or lefts > 1):
+            # msg.linear.x = 0
+            # msg.angular.z = 0.4
+            print("mover a la izquierda")
+        elif(rigth > 1 or rigths > 1):
+            # msg.linear.x = 0
+            # msg.angular.z = -0.4
+            print("mover a la derecha")
+        elif(left > 0 or lefts > 0):
+            # msg.linear.x = 0
+            # msg.angular.z = 0.8
+            print("mover a la izquierda")
+        elif(rigth > 0 or rigths > 0):
+            # msg.linear.x = 0
+            # msg.angular.z = -0.8
+            print("mover a la derecha")
+        else:
+            # msg.linear.x = 0.2
+            # msg.angular.z = 0
+            print("metale reversa")
         # rate.sleep()
         # self.publisher.publish(msg)
 
@@ -53,11 +81,11 @@ if __name__ == '__main__':
         image = ImageManager.load_image(img_name)
         parts = ImageManager.slice_image(image,9,12)
         p=[]
-        for j in range(108):
+        for j in range(84,108):
             x = ImageManager.retrieve_data(parts[j])
             p.append(int(turtlebot.svm.get_model().predict(np.asarray(x).reshape(1,13))))
         #print(p)
-        turtlebot.move(p)
+        turtlebot.move(p,84)
         cv2.imshow(img_name , image)
         cv2.waitKey()
         cv2.destroyAllWindows()
