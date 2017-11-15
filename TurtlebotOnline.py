d94ce899160f7631d8102700c7219aaced374e29
camilo <cam.912@hotmail.com>
2017-11-14 21:10:43 -0500

    codigo moverse ensayo

--- TurtlebotOnline.py
+++ TurtlebotOnline.py
@@ -1,5 +1,6 @@
 import rospy
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 from SvmModel import SvmModel
 from cv_bridge import CvBridge, CvBridgeError
@@ -41,7 +42,7 @@ class Turtlebot:
     def get_desired_values(self):
         return self.exp
 
-    def move(self, p, s):
+    def move(self, msg, p, s):
         msg = Twist()
         print(p[84-s:96-s])
         print(p[96-s:108-s])
@@ -50,48 +51,238 @@ class Turtlebot:
         right = sum(p[92-s:96-s])
         lefts = sum(p[96-s:100-s])
         rights = sum(p[104-s:108-s])
-        if central > 2:
-            msg.linear.x = 0.2
-            msg.angular.z = 0
-            self.pred.append(0)
-            print("Move forwards")
-        elif left > 2 or lefts > 2:
-            msg.linear.x = 0
-            msg.angular.z = 0.5
-            self.pred.append(3)
-            print("Move left")
-        elif right > 2 or rights > 2:
-            msg.linear.x = 0
-            msg.angular.z = -0.5
-            self.pred.append(1)
-            print("Move right")
-        elif left > 1 or lefts > 1:
-            msg.linear.x = 0
-            msg.angular.z = 0.8
-            self.pred.append(3)
-            print("Move left")
-        elif right > 1 or rights > 1:
-            msg.linear.x = 0
-            msg.angular.z = -0.8
-            self.pred.append(1)
-            print("Move right")
-        elif left > 0 or lefts > 0:
-            msg.linear.x = 0
-            msg.angular.z = 1.2
-            self.pred.append(3)
-            print("Move left")
-        elif right > 0 or rights > 0:
-            msg.linear.x = -0.2
-            msg.angular.z = 0
-            self.pred.append(1)
-            print("Move backwards")
+
+        destination = [0,10]
+        orientation = msg.pose.pose.orientation
+        goal = "unknown"
+        aim = "undefined"
+        if(orientation.z > -0.2 and orientation.z < 0.2):
+            aim = "up"
+        elif(orientation.z > -0.85 and orientation.z < -0.2):
+            aim = "left"
+        elif(orientation.z > -0.85):
+            aim = "back"
         else:
-            msg.linear.x = -0.2
-            msg.angular.z = 0
-            self.pred.append(2)
-            print("Move backwards")
-        rate.sleep()
-        self.publisher.publish(msg)
+            aim = "right"
+
+        if(abs(pose.pose.position.y - destination[1]) > abs(pose.pose.position.x - destination[0])):
+            if(pose.pose.position.y > destination[1]):
+                goal = "backwards"
+            else:
+                goal = "ahead"
+        else:
+            if(pose.pose.position.x > destination[0]):
+                goal = "left"
+            else:
+                goal = "right"
+
+        if goal == "ahead":
+            if central > 2:
+                msg.linear.x = 0.2
+                msg.angular.z = 0
+                self.pred.append(0)
+                print("Move forwards")
+            elif left > 2 or lefts > 2:
+                msg.linear.x = 0
+                msg.angular.z = 0.5
+                self.pred.append(3)
+                print("Move left")
+            elif right > 2 or rights > 2:
+                msg.linear.x = 0
+                msg.angular.z = -0.5
+                self.pred.append(1)
+                print("Move right")
+            elif left > 1 or lefts > 1:
+                msg.linear.x = 0
+                msg.angular.z = 0.8
+                self.pred.append(3)
+                print("Move left")
+            elif right > 1 or rights > 1:
+                msg.linear.x = 0
+                msg.angular.z = -0.8
+                self.pred.append(1)
+                print("Move right")
+            elif left > 0 or lefts > 0:
+                msg.linear.x = 0
+                msg.angular.z = 1.2
+                self.pred.append(3)
+                print("Move left")
+            elif right > 0 or rights > 0:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(1)
+                print("Move backwards")
+            else:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(2)
+                print("Move backwards")
+        elif goal == "rigth":
+            if right > 2 or rights > 2:
+                msg.linear.x = 0
+                msg.angular.z = -0.5
+                self.pred.append(1)
+                print("Move right")
+            elif central > 2:
+                msg.linear.x = 0.2
+                msg.angular.z = 0
+                self.pred.append(0)
+                print("Move forwards")
+            elif left > 2 or lefts > 2:
+                msg.linear.x = 0
+                msg.angular.z = 0.5
+                self.pred.append(3)
+                print("Move left")
+            elif right > 1 or rights > 1:
+                msg.linear.x = 0
+                msg.angular.z = -0.8
+                self.pred.append(1)
+                print("Move right")
+            elif left > 1 or lefts > 1:
+                msg.linear.x = 0
+                msg.angular.z = 0.8
+                self.pred.append(3)
+                print("Move left")
+            elif right > 0 or rights > 0:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(1)
+                print("Move rigth")
+            elif left > 0 or lefts > 0:
+                msg.linear.x = 0
+                msg.angular.z = 1.2
+                self.pred.append(3)
+                print("Move left")
+            else:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(2)
+                print("Move backwards")
+        elif goal == "left":
+            if left > 2 or lefts > 2:
+                msg.linear.x = 0
+                msg.angular.z = 0.5
+                self.pred.append(3)
+                print("Move left")
+            elif central > 2:
+                msg.linear.x = 0.2
+                msg.angular.z = 0
+                self.pred.append(0)
+                print("Move forwards")
+            elif right > 2 or rights > 2:
+                msg.linear.x = 0
+                msg.angular.z = -0.5
+                self.pred.append(1)
+                print("Move right")
+            elif left > 1 or lefts > 1:
+                msg.linear.x = 0
+                msg.angular.z = 0.8
+                self.pred.append(3)
+                print("Move left")
+            elif right > 1 or rights > 1:
+                msg.linear.x = 0
+                msg.angular.z = -0.8
+                self.pred.append(1)
+                print("Move right")
+            elif left > 0 or lefts > 0:
+                msg.linear.x = 0
+                msg.angular.z = 1.2
+                self.pred.append(3)
+                print("Move left")
+            elif right > 0 or rights > 0:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(1)
+                print("Move backwards")
+            else:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(2)
+                print("Move backwards")
+        else:
+            if left > 2 or lefts > 2:
+                msg.linear.x = 0
+                msg.angular.z = 0.5
+                self.pred.append(3)
+                print("Move left")
+            elif right > 2 or rights > 2:
+                msg.linear.x = 0
+                msg.angular.z = -0.5
+                self.pred.append(1)
+                print("Move right")
+            elif central > 2:
+                msg.linear.x = 0.2
+                msg.angular.z = 0
+                self.pred.append(0)
+                print("Move forwards")
+            elif left > 1 or lefts > 1:
+                msg.linear.x = 0
+                msg.angular.z = 0.8
+                self.pred.append(3)
+                print("Move left")
+            elif right > 1 or rights > 1:
+                msg.linear.x = 0
+                msg.angular.z = -0.8
+                self.pred.append(1)
+                print("Move right")
+            elif left > 0 or lefts > 0:
+                msg.linear.x = 0
+                msg.angular.z = 1.2
+                self.pred.append(3)
+                print("Move left")
+            elif right > 0 or rights > 0:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(1)
+                print("Move backwards")
+            else:
+                msg.linear.x = -0.2
+                msg.angular.z = 0
+                self.pred.append(2)
+                print("Move backwards")
+
+        # if central > 2:
+        #     msg.linear.x = 0.2
+        #     msg.angular.z = 0
+        #     self.pred.append(0)
+        #     print("Move forwards")
+        # elif left > 2 or lefts > 2:
+        #     msg.linear.x = 0
+        #     msg.angular.z = 0.5
+        #     self.pred.append(3)
+        #     print("Move left")
+        # elif right > 2 or rights > 2:
+        #     msg.linear.x = 0
+        #     msg.angular.z = -0.5
+        #     self.pred.append(1)
+        #     print("Move right")
+        # elif left > 1 or lefts > 1:
+        #     msg.linear.x = 0
+        #     msg.angular.z = 0.8
+        #     self.pred.append(3)
+        #     print("Move left")
+        # elif right > 1 or rights > 1:
+        #     msg.linear.x = 0
+        #     msg.angular.z = -0.8
+        #     self.pred.append(1)
+        #     print("Move right")
+        # elif left > 0 or lefts > 0:
+        #     msg.linear.x = 0
+        #     msg.angular.z = 1.2
+        #     self.pred.append(3)
+        #     print("Move left")
+        # elif right > 0 or rights > 0:
+        #     msg.linear.x = -0.2
+        #     msg.angular.z = 0
+        #     self.pred.append(1)
+        #     print("Move backwards")
+        # else:
+        #     msg.linear.x = -0.2
+        #     msg.angular.z = 0
+        #     self.pred.append(2)
+        #     print("Move backwards")
+        # rate.sleep()
+        # self.publisher.publish(msg)
 
     def shutdown(self):
         rospy.loginfo("Stopping Turtlebot")
@@ -131,6 +322,9 @@ class Turtlebot:
         cv2.moveWindow("debug", 710,20);
         cv2.imshow("debug", self.imageBaW)
 
+    def move_try(self, msg):
+        print(msg.pose.pose.position)
+
 if __name__ == '__main__':
     turtlebot = Turtlebot()
     rospy.init_node('Turtlebot', anonymous=False)
@@ -166,7 +360,8 @@ if __name__ == '__main__':
         for j in range(108):
             x2 = ImageManager.retrieve_data2(parts2[j], parts2_hsv[j], parts2_BaW[j])
             p2.append(int(turtlebot.svm.get_model().predict(np.asarray(x2).reshape(1, len(x2)))))
-        turtlebot.move(p2, 0)
+        #turtlebot.move(p2, 0)
+        rospy.Subscriber('odom', Odometry, turtlebot.move, callback_args=[p2,0])
         turtlebot.debug2(p2, turtlebot.image, segments)
         cv2.waitKey(5000)
         cv2.destroyAllWindows()
--- mapaPrueba/moverseConOdometria.py
+++ mapaPrueba/moverseConOdometria.py
@@ -3,116 +3,101 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
-fase = 0
+#fase = 0
+class Turtlebot:
 
-def response(msg, cont):
-    global fase
-    pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-    #rate = rospy.Rate(10)
-    #if((msg.pose.pose.position.x < -0.3 or msg.pose.pose.position.x > 0.7) or (msg.pose.pose.position.y < -0.7 or msg.pose.pose.position.y > 0.3)):
+    def __init__(self):
+        self.fase = 0
 
-    if(fase == 0):
-        if(msg.pose.pose.position.x < 2.5):
-            msg2 = Twist()
-            msg2.linear.x=0.3
-            msg2.angular.z=0.02
-            pub.publish(msg2)
-            print(msg.pose.pose.position.x)
-        else:
-            fase = 1
-    elif(fase == 1):
-        if(abs(msg.pose.pose.orientation.z) < 0.4):
-            msg2 = Twist()
-            msg2.angular.z=-1
-            pub.publish(msg2)
-            print(abs(msg.pose.pose.orientation.z))
-        else:
-            msg2 = Twist()
-            msg2.angular.z=0
-            pub.publish(msg2)
-            fase = 2
-    elif(fase == 2):
-        if(msg.pose.pose.position.y > -1.9):
-            msg2 = Twist()
-            msg2.linear.x=0.3
-            msg2.angular.z=0.02
-            pub.publish(msg2)
-            print(msg.pose.pose.position.y)
-        else:
-            fase = 3
-    elif(fase == 3):
-        if(abs(msg.pose.pose.orientation.z) < 0.95):
-            msg2 = Twist()
-            msg2.angular.z=-1
-            pub.publish(msg2)
-            print(abs(msg.pose.pose.orientation.z))
-        else:
-            msg2 = Twist()
-            msg2.angular.z=0
-            pub.publish(msg2)
-            fase = 4
-    elif(fase == 4):
-        if(msg.pose.pose.position.x > 0):
-            msg2 = Twist()
-            msg2.linear.x=0.3
-            msg2.angular.z=0.02
-            pub.publish(msg2)
-            print(msg.pose.pose.position.x)
-        else:
-            fase = 5
-    elif(fase == 5):
-        if(abs(msg.pose.pose.orientation.z) > 0.87):
-            msg2 = Twist()
-            msg2.angular.z=-1
-            pub.publish(msg2)
-            print(abs(msg.pose.pose.orientation.z))
-        else:
-            msg2 = Twist()
-            msg2.angular.z=0
-            pub.publish(msg2)
-            fase = 6
-    elif(fase == 6):
-        if(msg.pose.pose.position.y < 0):
-            msg2 = Twist()
-            msg2.linear.x=0.3
-            msg2.angular.z=0.02
-            pub.publish(msg2)
-            print(msg.pose.pose.position.y)
-        else:
-            fase = 7
+    def response(self, msg, cont):
+        pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        #rate = rospy.Rate(10)
+        #if((msg.pose.pose.position.x < -0.3 or msg.pose.pose.position.x > 0.7) or (msg.pose.pose.position.y < -0.7 or msg.pose.pose.position.y > 0.3)):
 
-    #rospy.sleep(0.01)
-    # if(msg.pose.pose.position.x < 2.8 and fase < 1):
-    #     fase = 0
-    #     msg2 = Twist()
-    #     msg2.linear.x=0.3
-    #     msg2.angular.z=0.02
-    #     pub.publish(msg2)
-    #     #print(msg.pose.pose.position.x, msg.pose.pose.position.y)
-    # elif(msg.pose.pose.orientation.w < -0.7 or msg.pose.pose.orientation.w > -0.6 or msg.pose.pose.orientation.z < 0.8 or msg.pose.pose.orientation.z > 0.7 and fase < 2):
-    #     fase = 1
-    #     msg2 = Twist()
-    #     msg2.angular.z=1
-    #     pub.publish(msg2)
-    #     #print(msg.pose.pose.orientation)
+        if(self.fase == 0):
+            if(msg.pose.pose.position.x < 2.5):
+                msg2 = Twist()
+                msg2.linear.x=0.3
+                msg2.angular.z=0.02
+                pub.publish(msg2)
+                print(msg.pose.pose.position.x)
+            else:
+                self.fase = 1
+        elif(self.fase == 1):
+            if(abs(msg.pose.pose.orientation.z) < 0.4):
+                msg2 = Twist()
+                msg2.angular.z=-1
+                pub.publish(msg2)
+                print(abs(msg.pose.pose.orientation.z))
+            else:
+                msg2 = Twist()
+                msg2.angular.z=0
+                pub.publish(msg2)
+                self.fase = 2
+        elif(self.fase == 2):
+            if(msg.pose.pose.position.y > -1.9):
+                msg2 = Twist()
+                msg2.linear.x=0.3
+                msg2.angular.z=0.02
+                pub.publish(msg2)
+                print(msg.pose.pose.position.y)
+            else:
+                self.fase = 3
+        elif(self.fase == 3):
+            if(abs(msg.pose.pose.orientation.z) < 0.95):
+                msg2 = Twist()
+                msg2.angular.z=-1
+                pub.publish(msg2)
+                print(abs(msg.pose.pose.orientation.z))
+            else:
+                msg2 = Twist()
+                msg2.angular.z=0
+                pub.publish(msg2)
+                self.fase = 4
+        elif(self.fase == 4):
+            if(msg.pose.pose.position.x > 0):
+                msg2 = Twist()
+                msg2.linear.x=0.3
+                msg2.angular.z=0.02
+                pub.publish(msg2)
+                print(msg.pose.pose.position.x)
+            else:
+                self.fase = 5
+        elif(self.fase == 5):
+            if(abs(msg.pose.pose.orientation.z) > 0.87):
+                msg2 = Twist()
+                msg2.angular.z=-1
+                pub.publish(msg2)
+                print(abs(msg.pose.pose.orientation.z))
+            else:
+                msg2 = Twist()
+                msg2.angular.z=0
+                pub.publish(msg2)
+                self.fase = 6
+        elif(self.fase == 6):
+            if(msg.pose.pose.position.y < 0):
+                msg2 = Twist()
+                msg2.linear.x=0.3
+                msg2.angular.z=0.02
+                pub.publish(msg2)
+                print(msg.pose.pose.position.y)
+            else:
+                self.fase = 7
 
-rospy.init_node('moveOdom', anonymous=False)
-cont = 0
-#while not rospy.is_shutdown():
-    #print(fase)
-sub = rospy.Subscriber('odom', Odometry, response, callback_args=cont)
-rospy.spin()
 
-# if __name__ == '__main__':
-#     rospy.init_node('moveOdom', anonymous=False)
-#     band = False
-#     cont = 0
-#     while not rospy.is_shutdown():
-#         if(not band):
-#             cont = 0
-#         else:
-#             cont = 1
-#         sub = rospy.Subscriber('odom', Odometry, response, callback_args=cont)
+if __name__ == '__main__':
+    rospy.init_node('moveOdom', anonymous=False)
+    cont = 0
+    turtlebot = Turtlebot()
+    sub = rospy.Subscriber('odom', Odometry, turtlebot.response, callback_args=cont)
+    rospy.spin()
+
+    # while not rospy.is_shutdown():
+    #     if(not band):
+    #         cont = 0
+    #     else:
+    #         cont = 1
+    #     sub = rospy.Subscriber('odom', Odometry, response, callback_args=cont)
         #print(sub)
     #response(sub)
     #print(response(sub))
